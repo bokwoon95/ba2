738ab9d11e178fbc34d3e6cf4e49654e46ae6b26
bokwoon <bokwoon.c@gmail.com>
2026-02-23 23:51:06 +0800

    checkpoint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import {Events} from "@wailsio/runtime";
 import {GreetService} from "../bindings/changeme";
+import "basecoat-css/basecoat";
+import "basecoat-css/sidebar";
+import "./dropdown-menu-v4.js";
 
 const resultElement = document.getElementById('result');
 const timeElement = document.getElementById('time');
@@ -17,5 +20,73 @@ window.doGreet = async () => {
 }
 
 Events.On('time', (time) => {
-    timeElement.innerText = time.data;
+    // timeElement.innerText = time.data;
+});
+
+/**
+ * @type {Record<string, (element: Element, attributeValue: string) => void>}
+ */
+const initFunctions = {
+  "data-click-event": function initClickEvent(targetElement, attributeValue) {
+    targetElement.addEventListener("click", function dispatchEvent() {
+      document.dispatchEvent(new Event(attributeValue));
+    });
+  },
+  "data-go-back": function initGoBack(targetElement) {
+    if (targetElement.tagName != "A") {
+      return;
+    }
+    targetElement.addEventListener("click", function goBack(event) {
+      if (!(event instanceof PointerEvent)) {
+        return;
+      }
+      if (document.referrer && history.length > 2 && !event.ctrlKey && !event.metaKey) {
+        event.preventDefault();
+        history.back();
+      }
+    });
+  },
+};
+const attributeNames = Object.keys(initFunctions);
+/**
+ * @param {Element} targetElement
+ */
+function initialize(targetElement) {
+  for (const attributeName of attributeNames) {
+    if (targetElement.hasAttribute(attributeName) && !targetElement.hasAttribute(attributeName + "-initialized")) {
+      try {
+        initFunctions[attributeName](targetElement, targetElement.getAttribute(attributeName));
+      } catch (e) {
+        console.error(e);
+      }
+      targetElement.setAttribute(attributeName + "-initialized", "");
+    }
+  }
+}
+const selector = attributeNames.map(name => "[" + name + "]").join(", ");
+for (const targetElement of document.querySelectorAll(selector)) {
+  initialize(targetElement);
+}
+const observer = new MutationObserver(function(mutationRecords) {
+  for (const mutationRecord of mutationRecords) {
+    if (mutationRecord.type != "childList") {
+      continue;
+    }
+    for (const addedElement of mutationRecord.addedNodes) {
+      if (!(addedElement instanceof Element)) {
+        continue;
+      }
+      initialize(addedElement);
+      for (const targetElement of targetElement.querySelectorAll(selector)) {
+        if (!(targetElement instanceof Element)) {
+          continue;
+        }
+        initialize(targetElement);
+      }
+    }
+  }
+});
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
 });
